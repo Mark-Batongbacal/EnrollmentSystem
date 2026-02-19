fd542af62dfc7db8f93cb0a2b74f62da4c330e86
Mark-Batongbacal <batongbacalmark@gmail.com>
2026-02-20 03:26:06 +0800

    Fixed studentservice to comply to the test

--- EnrollmentSystem/Services/Students/StudentService.cs
+++ EnrollmentSystem/Services/Students/StudentService.cs
@@ -5,42 +5,68 @@ namespace EnrollmentSystem.Services.Students
 {
     public class StudentService : IStudentService
     {
-        private readonly IStudentRepository _repository;
+        private readonly IStudentRepository _studentRepository;
 
-        public StudentService(IStudentRepository repository)
+        public StudentService(IStudentRepository studentRepository)
         {
-            _repository = repository;
+            _studentRepository = studentRepository;
+        }
+
+        public async Task<IEnumerable<Student>> GetAllAsync()
+        {
+            return await _studentRepository.GetAllAsync();
+        }
+
+        public async Task<Student?> GetByIdAsync(int id)
+        {
+            return await _studentRepository.GetByIdAsync(id);
         }
 
         public async Task<Student> CreateAsync(Student student)
         {
-            await _repository.AddAsync(student);
+            student.DateCreated = DateOnly.FromDateTime(DateTime.UtcNow);
+
+            if (_studentRepository.ExistsAsync(student.StudentNumber).Result)
+            {
+                throw new InvalidOperationException($"A student with StudentNumber {student.StudentNumber} already exists.");
+            }
+            await _studentRepository.AddAsync(student);
+
             return student;
         }
 
-        public Task<bool> DeleteAsync(int id)
+        public async Task<bool> UpdateAsync(int id, Student updatedStudent)
         {
-            throw new NotImplementedException();
+            var existingStudent = await _studentRepository.GetByIdAsync(id);
+
+            if (existingStudent == null)
+                return false;
+
+            existingStudent.StudentNumber = updatedStudent.StudentNumber;
+            existingStudent.FirstName = updatedStudent.FirstName;
+            existingStudent.LastName = updatedStudent.LastName;
+            existingStudent.Email = updatedStudent.Email;
+
+            await _studentRepository.Update(existingStudent);
+
+            return true;
         }
 
-        public Task<bool> ExistsAsync(int id)
+        public async Task<bool> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var student = await _studentRepository.GetByIdAsync(id);
+
+            if (student == null)
+                return false;
+
+            await _studentRepository.Delete(student);
+
+            return true;
         }
 
-        public Task<IEnumerable<Student>> GetAllAsync()
+        public async Task<bool> ExistsAsync(int id)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<Student?> GetByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> UpdateAsync(int id, Student student)
-        {
-            throw new NotImplementedException();
+            return await _studentRepository.ExistsAsync(id);
         }
     }
 }
